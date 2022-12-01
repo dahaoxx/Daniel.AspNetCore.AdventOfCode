d59e6b3e1a394dd1baea90536db7719b5a63d7da
Daniel Roger Hansen <daniel.brann@hotmail.no>
2022-12-01 21:23:57 +0100

    Initial Commit

---
+++ Tests/AdventOfCode.One.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.One.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
