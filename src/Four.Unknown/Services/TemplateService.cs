c08ab99c61a32efd2ad0648636fcd4d1111b8d05
Daniel Roger Hansen <daniel.brann@hotmail.no>
2022-12-03 13:23:44 +0100

    Add Day 4 Template

---
+++ src/Four.Unknown/Services/TemplateService.cs
@@ -0,0 +1,24 @@
+﻿namespace Four.Unkown.Services;
+
+public static class TemplateService
+{
+    #region Part 1
+
+    public static int Part1(string inputFileName)
+    {
+        return 1;
+    }
+    
+  
+
+    #endregion
+    
+    #region Part 2
+    
+    public static int Part2(string inputFileName)
+    {
+        return 2;
+    }
+    
+    #endregion
+}
