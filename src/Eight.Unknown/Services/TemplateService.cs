91b2f971bd653d6d82dfb80cf687f29bee16b168
Daniel Roger Hansen <daniel.hansen@oxx.no>
2022-12-08 16:19:12 +0100

    Solve day 8 - Part 2

--- src/Eight.Unknown/Services/TemplateService.cs
+++ src/Eight.Unknown/Services/TemplateService.cs
@@ -9,13 +9,21 @@ public static class TemplateService
         var lines = File.ReadAllLines(inputFileName);
         var treeMatrix = CreateTreeMatrix(lines);
 
-        return treeMatrix
-            .Select((treeRow, rowIndex) 
-                => treeRow
-                    .Select((tree, columnIndex) 
-                        => IsVisible(tree, rowIndex, columnIndex, treeMatrix))
-                    .Count(isVisible => isVisible))
-            .Sum();
+        var visibleTrees = 0;
+        for (var rowIndex = 0; rowIndex < treeMatrix.Count; rowIndex++)
+        {
+            var treeRow = treeMatrix[rowIndex];
+            for (var columnIndex = 0; columnIndex < treeRow.Count; columnIndex++)
+            {
+                var isVisible = IsVisible(treeRow[columnIndex], rowIndex, columnIndex, treeMatrix);
+                if (isVisible)
+                {
+                    visibleTrees++;
+                }
+            }
+        }
+
+        return visibleTrees;
     }
     
     private static List<List<int>> CreateTreeMatrix(IEnumerable<string> lines) 
@@ -90,7 +98,90 @@ public static class TemplateService
     
     public static int Part2(string inputFileName)
     {
-        return 2;
+        var lines = File.ReadAllLines(inputFileName);
+        var treeMatrix = CreateTreeMatrix(lines);
+
+        var highestScenicScore = 0;
+        for (var rowIndex = 0; rowIndex < treeMatrix.Count; rowIndex++)
+        {
+            var treeRow = treeMatrix[rowIndex];
+            for (var columnIndex = 0; columnIndex < treeRow.Count; columnIndex++)
+            {
+                var currentScenicScore = GetScenicScore(treeRow[columnIndex], rowIndex, columnIndex, treeMatrix);
+                if (currentScenicScore > highestScenicScore)
+                {
+                    highestScenicScore = currentScenicScore;
+                }
+            }
+        }
+
+        return highestScenicScore;
+    }
+
+    private static int GetScenicScore(int currentTree, int rowIndex, int columnIndex, IReadOnlyList<List<int>> treeMatrix)
+    {
+        return ScenicScoreInNorth(currentTree, rowIndex, columnIndex, treeMatrix) *
+               ScenicScoreInEast(currentTree, rowIndex, columnIndex, treeMatrix) *
+               ScenicScoreInSouth(currentTree, rowIndex, columnIndex, treeMatrix) *
+               ScenicScoreInWest(currentTree, rowIndex, columnIndex, treeMatrix);
+    }
+
+
+    private static int ScenicScoreInNorth(int tree, int targetRowIndex, int targetColumnIndex, IReadOnlyList<List<int>> treeMatrix)
+    {
+        var currentScenicScore = 0;
+        for (var currentRowIndex = targetRowIndex - 1; currentRowIndex >= 0; currentRowIndex--)
+        {
+            currentScenicScore++;
+            var currentTree = treeMatrix[currentRowIndex][targetColumnIndex];
+            if (currentTree < tree) continue;
+            
+            return currentScenicScore;
+        }
+        return currentScenicScore;
+    }
+    
+    private static int ScenicScoreInEast(int tree, int targetRowIndex, int targetColumnIndex, IReadOnlyList<List<int>> treeMatrix)
+    {
+        var currentScenicScore = 0;
+        var targetRow = treeMatrix[targetRowIndex];
+        for (var currentColumnIndex = targetColumnIndex + 1; currentColumnIndex < targetRow.Count; currentColumnIndex++)
+        {
+            currentScenicScore++;
+            var currentTree = treeMatrix[targetRowIndex][currentColumnIndex];
+            if (currentTree < tree) continue;
+            
+            return currentScenicScore;
+        }
+        return currentScenicScore;
+    }
+    
+    private static int ScenicScoreInSouth(int tree, int targetRowIndex, int targetColumnIndex, IReadOnlyList<List<int>> treeMatrix)
+    {
+        var currentScenicScore = 0;
+        for (var currentRowIndex = targetRowIndex + 1; currentRowIndex < treeMatrix.Count; currentRowIndex++)
+        {
+            currentScenicScore++;
+            var currentTree = treeMatrix[currentRowIndex][targetColumnIndex];
+            if (currentTree < tree) continue;
+            
+            return currentScenicScore;
+        }
+        return currentScenicScore;
+    }
+    
+    private static int ScenicScoreInWest(int tree, int targetRowIndex, int targetColumnIndex, IReadOnlyList<List<int>> treeMatrix)
+    {
+        var currentScenicScore = 0;
+        for (var currentColumnIndex = targetColumnIndex - 1; currentColumnIndex >= 0; currentColumnIndex--)
+        {
+            currentScenicScore++;
+            var currentTree = treeMatrix[targetRowIndex][currentColumnIndex];
+            if (currentTree < tree) continue;
+            
+            return currentScenicScore;
+        }
+        return currentScenicScore;
     }
     
     #endregion
